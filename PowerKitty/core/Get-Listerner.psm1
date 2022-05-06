d400d6fae21f9719f047641ad54b840e98efa126
Will Frampton <will@bambi.lol>
2022-05-06 23:48:19 +0800

    .

--- PowerKitty/core/Get-Listerner.psm1
+++ PowerKitty/core/Get-Listerner.psm1
@@ -3,14 +3,26 @@
 #
 Function Global:Get-Listerner()
 {
-
 	param
 	(
 		[Parameter()]
-		[String]$UID
+		[String]$UID,
+
+		[Parameter()]
+		[String]$Name,
+
+		[Parameter()]
+		[String]$LHOST,
+
+		[Parameter()]
+		[long]$LPORT,
 
 	)
 
-	$Global:ListernerPool | Where-Object {$_.UUID -like "*$UID*"}
-
+	$Global:ListernerPool | Where-Object {
+		$_.UUID -like "*$UID*" -and
+		$_.Name -like "*$Name*" -and
+		$_.LHOST -like "*$LHOST*" -and
+		$_.LPORT -like "*$LPORT*" 
+	}
 }
