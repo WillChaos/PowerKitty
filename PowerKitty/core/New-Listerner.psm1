a20d19966e4976c97d5ce347fddf2bb23abb2e86
Will Frampton <will@bambi.lol>
2022-05-06 22:22:11 +0800

    listerner testing

--- PowerKitty/core/New-Listerner.psm1
+++ PowerKitty/core/New-Listerner.psm1
@@ -15,19 +15,43 @@ Function Global:New-Listerner()
 		[short]$LPORT
 	)
 
+	# If global lister object doesnt exist,or is null - create one
+	if(!($Global:ListernerPool)){
+		$Global:ListernerPool = @()
+	}
+
 	# Create a template listerner object
-	[pscustomobject]@{
-		Name = $ListerName
-		UUID = (New-Guid).Guid
+	[pscustomobject]$obj@{
+		Name  = $ListerName
+		UUID  = (New-Guid).Guid
 		LHOST = $LHOST
 		LPORT = $LPORT
 	}
-	# Build a simple TCP listerner
-	[console]::Title = ("Server: $LHOST : $LPORT")
-	$endpoint = new-object System.Net.IPEndPoint ($LHOST, $LPORT)
-	$listener = new-object System.Net.Sockets.TcpListener $endpoint
-	$listener.start()
-	$client = $listener.AcceptTcpClient()
+
+	# add listerner psobject to array
+	$Global:ListernerPool.Add($obj)
+
+	# build a runspace
+	$Runspace            = [runspacefactory]::CreateRunspace()
+	$PowerShell          = [powershell]::Create()
+	$PowerShell.runspace = $Runspace
+	$Runspace.Open()
+
+	[void]$PowerShell.AddScript({
+
+		# Build a simple TCP listerner
+		[console]::Title = ("Server: $LHOST : $LPORT")
+		$endpoint        = new-object System.Net.IPEndPoint ($LHOST, $LPORT)
+		$listener        = new-object System.Net.Sockets.TcpListener $endpoint
+		$listener.start()
+		$client          = $listener.AcceptTcpClient()
+
+	})
+
+	$AsyncObject = $PowerShell.BeginInvoke()
+	$AsyncObject
+
+	
 
 	
 }
