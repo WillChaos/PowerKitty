ca7f518841a790b5e947bbc15f7fcbf7bc647775
Will Frampton <will@bambi.lol>
2022-05-06 22:41:52 +0800

    .

--- PowerKitty/core/New-Listerner.psm1
+++ PowerKitty/core/New-Listerner.psm1
@@ -41,7 +41,7 @@ Function Global:New-Listerner()
 
 		# Build a simple TCP listerner
 		[console]::Title = ("Server: $LHOST : $LPORT")
-		$endpoint        = new-object System.Net.IPEndPoint ($LHOST, $LPORT)
+		$endpoint        = new-object System.Net.IPEndPoint ([system.net.ipaddress]$LHOST, $LPORT)
 		$listener        = new-object System.Net.Sockets.TcpListener $endpoint
 		$listener.start()
 		$client          = $listener.AcceptTcpClient()
